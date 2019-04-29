c9bc535198cedc45c39b3cf39451f5ee26b621a3
Miles Salunga <49493428+MilesSalunga@users.noreply.github.com>
2019-04-29 16:10:21 -0700

    added rectangle.h

--- rectangle.h
+++ rectangle.h
@@ -1,4 +1,24 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
+#include "fillableshape.h"
+
+class Rectangle : public fillableShape
+{
+public:
+    Rectangle(QPoint, QPoint, fillProperties, borderProperties);
+    Rectangle(QPoint, int, int, fillProperties, borderProperties);
+
+    void draw() override;
+
+    int getWidth() const;
+    int getHeight() const;
+
+    void setWidth(int);
+    void setHeight(int);
+private:
+    int width;
+    int height;
+};
+
 #endif // RECTANGLE_H
